d3305fbfd99df10c5a4103d9ca79dadcdc304a2c
ncclapyohands <nathancook648@gmail.com>
2022-04-24 00:49:28 -0600

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let lastName = 'Dylan';
+let firstName = 'Israel';
+//const is just a constant variable
+const fullName = '${firstName} ${lastName}';
+const daysUntil = 'There are ${daycount} days until ${targetEvent}!';
+
+console.log('${firstName} ${lastName}'.split(' '));
+
+let example = 7;
+
+console.log(parseInt(example.toFixed(1)));
+
+let array = [5, 57, 6];
+
+console.log(array[1]);
+
+// push adds the number to the end of the array
+example.push
+
+<!--This is an HTML comment-->
