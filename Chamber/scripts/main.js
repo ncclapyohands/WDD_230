9c9c7a37058da7ae9e24085253567640ed513111
ncclapyohands <nathancook648@gmail.com>
2022-06-10 21:22:32 -0600

    week 8 table

--- Chamber/scripts/main.js
+++ Chamber/scripts/main.js
@@ -1,8 +1,8 @@
-//local storage
-localStorage.setItem('msg1', 'Hello Permanant');
-sessionStorage.setItem('msg2','hello again');
+// //local storage
+// localStorage.setItem('msg1', 'Hello Permanant');
+// sessionStorage.setItem('msg2','hello again');
 
-console.log(localStorage.getItem('msg1'));
+// console.log(localStorage.getItem('msg1'));
 
 
 
@@ -11,7 +11,7 @@ let visit_heading = document.querySelector('.visited');
 let now_date = new Date();
 
 
-let stored_date = localStorage(getItem('last_visited'));
+let stored_date = localStorage.getItem('last_visited');
 
 let last_date = new Date(stored_date);
 
@@ -26,5 +26,25 @@ else{
     visit_heading.textContent = diff_day + ' days since your last visit';
 }
 
+// // Hamburger menu
+// localStorage.setItem('last_visited', now_date);
 
-localStorage.setItem('last_visited', now_date);
+// let button1 = document.querySelector('.btn');
+
+
+// function show() {
+// button1.classList.toggle('newstyle');
+// }
+
+// button1.addEventListener('click', show);
+
+
+// Todays Date
+
+let tdate = new Date();
+const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(tdate);
+document.querySelector('#date-today').textContent = fullDate;
+
+
+
+
---
+++ Week8/scripts/form.js
@@ -0,0 +1,5 @@
+let email_date = document.querySelector('#date');
+
+let date = new Date();
+
+email_date.value = date;
